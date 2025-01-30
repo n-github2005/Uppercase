0bb7e777cf8e628149223c1fc5640e2cbb6c3708
n-github2005 <kadudulanamitha@gmail.com>
2025-01-30 22:24:57 +0530

    Add files via upload

---
+++ Uppercase.java
@@ -0,0 +1,8 @@
+public class Uppercase {
+    public static void main(String[] args) {
+        String str="Hello World";
+        String upperstr=str.toUpperCase();
+        System.out.println("Uppercase:"+upperstr);
+    }
+    
+}
